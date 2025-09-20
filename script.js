40f4e3d3a9ed91be2cf013e4c6231bdc874b9cee
senmq <rifkyapc@gmail.com>
2025-09-20 23:41:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showForm(formId) {
+  document
+    .querySelectorAll(".form-box")
+    .forEach((form) => form.classList.remove("active"));
+
+  document.getElementById(formId).classList.add("active");
+}
